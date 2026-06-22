3a3c9261eefa2a6cc6f9c8e47acbf27452f452ed
JosueFP <josue@mail.com>
2026-06-22 19:44:04 -0400

    tarea: agregar gestión de estados para tesis

---
+++ app/Http/Requests/UpdateThesisStatusRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateThesisStatusRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'status' => ['required', 'string', 'in:revision,aprobado,rechazado'],
+        ];
+    }
+}
